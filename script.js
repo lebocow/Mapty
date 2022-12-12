f69c09d901cc1e1ccea26d31ddfbdb7385a349df
Ursachianu Ionut-Alexandru <tzmic1@gmail.com>
2022-12-12 16:47:16 +0200

    Refactoring code using oop

--- script.js
+++ script.js
@@ -11,67 +11,81 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude, longitude } = position.coords;
-      console.log(latitude, longitude);
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    form.addEventListener(`submit`, this._newWorkout.bind(this));
 
-      const coords = [latitude, longitude];
+    inputType.addEventListener(`change`, this._toggleElevationField());
+  }
 
-      map = L.map('map').setView(coords, 13);
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert(`Could not get you position`);
+        }
+      );
+  }
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    console.log(latitude, longitude);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-      //   L.marker(coords)
-      //     .addTo(map)
-      //     .bindPopup('Your current location')
-      //     .openPopup();
+    const coords = [latitude, longitude];
 
-      // Handling clicks on map
-      map.on(`click`, function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove(`hidden`);
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert(`Could not get you position`);
-    }
-  );
+    this.#map = L.map('map').setView(coords, 13);
 
-form.addEventListener(`submit`, function (e) {
-  e.preventDefault();
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-  // Clear input fields
-  inputDistance.value = '';
-  inputDuration.value = '';
-  inputCadence.value = '';
-  inputElevation.value = '';
+    // Handling clicks on map
+    this.#map.on(`click`, this._showform.bind(this));
+  }
 
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: `running-popup`,
-      })
-    )
-    .setPopupContent(`Workout`)
-    .openPopup();
-});
+  _showform(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove(`hidden`);
+    inputDistance.focus();
+  }
 
-inputType.addEventListener(`change`, function (e) {
-  inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`);
-  inputCadence.parentElement.classList.toggle(`form__row--hidden`);
-});
+  _toggleElevationField() {
+    inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+    inputCadence.parentElement.classList.toggle(`form__row--hidden`);
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value = '';
+    inputDuration.value = '';
+    inputCadence.value = '';
+    inputElevation.value = '';
+
+    //Display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `running-popup`,
+        })
+      )
+      .setPopupContent(`Workout`)
+      .openPopup();
+  }
+}
+
+const app = new App();
